1458888025bfd0b5d2d578a61b9da99aa96209af
JOAO RIBEIRO <jocasiribeiro@hotmail.com>
2022-02-17 19:48:37 -0300

    Criação das entidades

---
+++ QuickBuy.Dominio/ObjetpDeValor/FormaPagamento.cs
@@ -0,0 +1,29 @@
+﻿using QuickBuy.Dominio.Enumerados;
+
+namespace QuickBuy.Dominio.ObjetpDeValor
+
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool EhBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhPix
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Pix; }
+        }
+        public bool EhNaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+
+    }
+}
